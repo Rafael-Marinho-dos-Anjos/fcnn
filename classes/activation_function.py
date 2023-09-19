f4ae5bebe015d8962033c2471d58e420f0256328
Rafael-Marinho-dos-Anjos <rafaelmarinhoanjos@gmail.com>
2023-09-19 12:34:42 -0300

    initial commit

---
+++ classes/activation_function.py
@@ -0,0 +1,8 @@
+""" Activation functions module
+"""
+
+from interfaces.activation_function_interface import ActivationFunctionInterface
+
+
+class ActivationFunction(ActivationFunctionInterface):
+    pass
---
+++ interfaces/activation_function_interface.py
@@ -0,0 +1,41 @@
+""" Activation function interface module
+"""
+
+from abc import ABC, abstractmethod
+from typing import List
+
+
+class ActivationFunctionInterface(ABC):
+    @abstractmethod
+    def calculate(input: float) -> float:
+        """
+        Calculates the value of the neuron output after
+        passing by the activation function.
+        :param float input: input of the Activation function (neuron output)
+        :return: float
+        """
+    
+    @abstractmethod
+    def derivate(input: float) -> float:
+        """
+        Calculates the derivate value of function at the given
+        point (used for backpropagation).
+        :param float input: derivation point
+        :return: float
+        """
+
+    @abstractmethod
+    def backpropagate() -> float:
+        """
+        Automaticly calculate the backpropagation value for neuron
+        with the stored gradient values.
+        :return: float
+        """
+
+    @abstractmethod
+    def auto_grad(activate: bool = True) -> None:
+        """
+        Activate or deactivate the gradient auto storage (used for
+        a faster backpropagation).
+        :param bool activate: activates autograd if True (deactivates if False)
+        """
